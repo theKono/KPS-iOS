80da7962f9a451dc1ec5b3fdbdb57135be6527f9
mingshing <kuokuo321@gmail.com>
2021-07-18 15:30:26 +0800

    Initial Commit

---
+++ KPS/KPS.h
@@ -0,0 +1,18 @@
+//
+//  KPS.h
+//  KPS
+//
+//  Created by mingshing on 2021/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KPS.
+FOUNDATION_EXPORT double KPSVersionNumber;
+
+//! Project version string for KPS.
+FOUNDATION_EXPORT const unsigned char KPSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KPS/PublicHeader.h>
+
+
