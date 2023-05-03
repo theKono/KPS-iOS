39a7386c76bea3f79637c93405b3f2a8fd6e9712
angelcic <xuanchen@hotmail.com.tw>
2023-05-03 17:54:32 +0800

    [Refactor] Refactor audio content data parsing

--- KPS/Model/KPSAudioResource.swift
+++ KPS/Model/KPSAudioResource.swift
@@ -11,9 +11,10 @@ public struct KPSAudioResource: Decodable {
     public let type: String
     public let duration: Double
     public let streamingUrl: String
+    public var sentences: [[String: Any]] = []
 
     enum CodingKeys: String, CodingKey {
-        case type, duration, streamingUrl
+        case type, duration, streamingUrl, sentences
     }
 
 }
@@ -32,6 +33,15 @@ extension KPSAudioResource {
             duration = Double(durationInt)
         }
         streamingUrl = try container.decode(String.self, forKey: .streamingUrl)
+        
+        var sentencesRes: [[String: Any]] = []
+        let tempSentences = try container.decodeIfPresent([Any].self, forKey: .sentences) ?? []
+        for tempSentence in tempSentences {
+            if let sentence = tempSentence as? [String: Any] {
+                sentencesRes.append(sentence)
+            }
+        }
+        sentences = sentencesRes
     }
 }
 
